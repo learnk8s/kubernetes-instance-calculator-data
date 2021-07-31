f180ee3063854200ad53db3dc6d1d254e58b1902
Hussein Feras <husseinferas12@gmail.com>
2021-07-31 23:20:17 +0300

    aws cli get inctances info

--- aws.js
+++ aws.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
-
 const region = "us-east-1";
 const cloud = "aws"
+const inputFile = "./aws.json";
 const outputFile = "instances.json";
 const memType = "binary"
+const cmd=require('node-cmd');
 
-let instances = [
-    {
-        id: "n1-standard-2",
-        name: "n1-standard-2",
-        os: { memory: { value: 100, type: memType }, cpu: 100 },
-        kubelet: { memory: { value: 100, type: memType }, cpu: 100 },
-        evictionThreshold: { memory: { value: 100, type: memType }, cpu: 0 },
-        totalMemory: { value: 7500, type: memType },
-        totalCpu: 2000,
-        costPerHour: 0.0949995,
-        provisioningTime: 12000,
-        maxPodCount: 110,
-        cloudProvider: cloud,
-    }
-];
+//get the input data which generate by aws cli aws ec2 describe-instance-types --instance-types > aws.json
+let input = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
+let instances = [];
+input = input.InstanceTypes;
+
+for (let i = 0; i < input.length; i++) {
+    let maxPodCount = cmd.runSync('bash max-pods-calculator.sh --cni-version 1.7.5  --instance-type '+input[i].InstanceType).data.trim()
+    instances.push([
+        {
+            id: input[i].InstanceType,
+            name: input[i].InstanceType,
+            os: { memory: { value: 100, type: memType }, cpu: 100 }, //TODO calculate
+            kubelet: { memory: { value: 100, type: memType }, cpu: 100 },//TODO calculate
+            evictionThreshold: { memory: { value: 100, type: memType }, cpu: 0 },//TODO calculate
+            totalMemory: { value: input[i].MemoryInfo.SizeInMiB, type: memType },
+            totalCpu: input[i].VCpuInfo.DefaultVCpus,
+            costPerHour: 0.0949995,//TODO find
+            provisioningTime: 12000,
+            maxPodCount: maxPodCount,
+            cloudProvider: cloud,
+        }
+    ]);
+}
 
 
 
