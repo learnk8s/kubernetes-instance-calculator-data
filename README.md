c004d7d87b150a4e0117ea64630ba6cde39eccd9
Daniele Polencic <daniele.polencic@gmail.com>
2021-10-20 16:34:30 +0800

    refactored cli

--- README.md
+++ README.md
@@ -3,30 +3,37 @@
 Export a list of instances with:
 
 ```bash
-node run.js [aws|gcp|azure|all]
+node run.js crunch
 ```
 
 #### generate the input files
-````bash
+
+```bash
 aws ec2 describe-instance-types --instance-types > input/aws.json
-````
-````bash
+```
+
+```bash
 gcloud compute machine-types list  --filter="zone:us-east1-b" > input/gcp.txt
-````
-````bash
+```
+
+```bash
 az vm list-sizes --location eastus > input/azure.json
-````
+```
+
 #### generate the pricing files
-````bash
-download csv from https://instances.vantage.sh/ and convert to json file 
-````
-````bash
+
+```bash
+download csv from https://instances.vantage.sh/ and convert to json file
+```
+
+```bash
 wget https://cloudpricingcalculator.appspot.com/static/data/pricelist.json -O input/gcp-pricing.json
-````
-````bash
+```
+
+```bash
 download json from https://azureprice.net/
-````
+```
 
 #### Calculator
 
-Please check our artical https://learnk8s.io/kubernetes-instance-calculator 
+Please check the Kubernetes instance calculator that uses the raw data https://learnk8s.io/kubernetes-instance-calculator
--- run.js
+++ run.js
@@ -11,92 +11,106 @@ const azurePricing = require("./input/azure-pricing.json");
 const gcpPricing = require("./input/gcp-pricing.json");
 const awsPricing = require("./input/aws-pricing.json");
 
-const output="instances.json";
+const { program } = require("commander");
 
-const args = process.argv
-  .slice(2)
-  .map((it) => it.trim().toLowerCase())
-  .filter((it) => ['aws', 'gcp', 'azure', 'all-cloud', 'data', 'time', 'pricing', 'all-tasks'].includes(it))
-  .filter(onlyUnique);
+program
+  .command("crunch")
+  .option("-o, --output <file name>", "Output file name.", "instances.json")
+  .option(
+    "-c, --cloud-providers <providers...>",
+    "Includes only those providers. Default: all",
+    "all"
+  )
+  .option("-t, --time", "Includes the provisioning time for each compute unit.")
+  .action((options) => {
+    const cloudProviders = (
+      Array.isArray(options.cloudProviders)
+        ? options.cloudProviders
+        : [options.cloudProviders]
+    )
+      .map((it) => it.toLowerCase())
+      .filter((it) => ["aws", "gcp", "azure", "all"].includes(it));
 
-const allowedCloudProviders = ['aws', 'gcp', 'azure'];
-const cloudProviders =
-    args.length === 0 || args.includes('all-cloud')
-        ? allowedCloudProviders
-        : args.filter((cloud) => allowedCloudProviders.includes(cloud));
-
-const allowedTasks = ['data', 'time', 'pricing'];
-const tasks =
-    args.length === 0 || args.includes('all-tasks')
-        ? allowedTasks
-        : args.filter((cloud) => allowedTasks.includes(cloud));
-
-const instances = cloudProviders
-  .reduce((acc, it) => {
-    switch (it) {
-      case "gcp":
-        return [
-          ...acc,
-          ...getGCPInstances(
-            fs.readFileSync(gcpInstances, "utf-8"),
-            gcpPricing.gcp_price_list,
-            tasks
-          ),
-        ];
-      case "aws":
-        return [
-          ...acc,
-          ...getAWSInstances(awsInstances.InstanceTypes, awsPricing,tasks),
-        ];
-      case "azure":
-        return [
-          ...acc,
-          ...getAzureInstances(azureInstances, azurePricing.data,tasks),
-        ];
-      default:
+    const instances = (
+      cloudProviders.includes("all") ? ["aws", "gcp", "azure"] : cloudProviders
+    )
+      .reduce((acc, it) => {
+        switch (it) {
+          case "gcp":
+            return [
+              ...acc,
+              ...getGCPInstances(
+                fs.readFileSync(gcpInstances, "utf-8"),
+                gcpPricing.gcp_price_list,
+                !!options.time
+              ),
+            ];
+          case "aws":
+            return [
+              ...acc,
+              ...getAWSInstances(
+                awsInstances.InstanceTypes,
+                awsPricing,
+                !!options.time
+              ),
+            ];
+          case "azure":
+            return [
+              ...acc,
+              ...getAzureInstances(
+                azureInstances,
+                azurePricing.data,
+                !!options.time
+              ),
+            ];
+          default:
+            return acc;
+        }
+      }, [])
+      .reduce((acc, it) => {
+        acc[it.id] = it;
         return acc;
+      }, {});
+
+    if (Object.values(instances).some((it) => isNull(it.costPerHour))) {
+      console.log(
+        `Invalid prices for:\n${Object.values(instances)
+          .filter((it) => isNull(it.costPerHour))
+          .map((it) => `- ${it.name} (${it.cloudProvider})`)
+          .join("\n")}`
+      );
     }
-  }, [])
-  .reduce((acc, it) => {
-    acc[it.id] = it;
-    return acc;
-  }, {});
 
-if (Object.values(instances).some((it) => isNull(it.costPerHour)) && tasks.includes('pricing')) {
-  console.log(
-    `Invalid prices for:\n${Object.values(instances)
-      .filter((it) => isNull(it.costPerHour))
-      .map((it) => `- ${it.name} (${it.cloudProvider})`)
-      .join("\n")}`
-  );
-}
-
-if (
-  Object.values(instances)
-    .map((it) => it.id)
-    .filter(onlyUnique).length !== Object.values(instances).length
-) {
-  console.log("Collisions in the IDs");
-}
-
-if (Object.values(instances).length > 0) {
-  const content = Object.values(instances).reduce((acc, it) => {
-    if (!isNull(it.costPerHour) && tasks.includes('pricing')) {
-      acc[it.id] = it;
+    if (
+      Object.values(instances)
+        .map((it) => it.id)
+        .filter(onlyUnique).length !== Object.values(instances).length
+    ) {
+      console.log("Collisions in the IDs");
     }
-    return acc;
-  }, {});
-  fs.writeFileSync(output, JSON.stringify(content), "utf8");
-  console.log(`output has been written to ${output}`)
-} else {
-  console.log("No instances exported.");
-}
 
-const longestName = Math.max.apply(
-  null,
-  Object.values(instances).map((it) => it.name.length)
-);
-console.log(`The longest instance name has ${longestName} characters`);
+    if (Object.values(instances).length > 0) {
+      const content = Object.values(instances).reduce((acc, it) => {
+        if (!isNull(it.costPerHour)) {
+          acc[it.id] = it;
+        }
+        return acc;
+      }, {});
+      fs.writeFileSync(options.output, JSON.stringify(content), "utf8");
+      console.log(`output has been written to ${options.output}`);
+    } else {
+      console.log("No instances exported.");
+      return;
+    }
+
+    const longestName = Math.max.apply(
+      null,
+      Object.values(instances).map((it) => it.name.length)
+    );
+    console.log(`The longest instance name has ${longestName} characters`);
+  });
+
+program.parse(process.argv);
 
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
